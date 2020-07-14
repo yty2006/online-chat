f07f0e59628d6ba3d7da3eeaf15a0b1e4fc3d886
yty2006 <67256743+yty2006@users.noreply.github.com>
2020-07-14 19:08:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,5 @@
 <frameset rows="70%,*" BORDER="0"> 
 <frame name="top" src="_b.php" marginwidth="0" marginheight="0" scrolling="yes" FRAMEBORDER="NO" noresize> 
 <frame name="bottom" src="_a.php" marginwidth="0" marginheight="0" scrolling="no" frameborder="no" noresize> 
-<a href="../">返回</a>
 </frameset> 
-<body><a href="../">返回</a></body> 
 </html> 
